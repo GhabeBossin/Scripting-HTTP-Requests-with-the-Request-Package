e3d604bce4816d38025f7ecf5fa22be0099469a7
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-15 04:24:20 +0000

    remove commented logs, add semicolon

--- request_image.js
+++ request_image.js
@@ -5,14 +5,13 @@ request.get('https://sytantris.github.io/http-examples')               // Note 1
       .on('error', function (err) {                                    // Note 2
         throw err;
       })
-      .on('response', function (response) {  
-        console.log('Response Status Code: ' + response.statusCode + '\n' + 'Response Message: ' + response.statusMessage + '\n' + 'Content Type: ' + response.headers['content-type'])
-        console.log('Downloading image...')                    // Note 3
-        // console.log('Response Status Code: ', response.statusCode)
-        // console.log('Response Message: ', response.statusMessage)
-        // console.log('Content Type: ', response.headers['content-type'])
+      .on('response', function (response) {
+        console.log('Response Status Code: ' + response.statusCode + '\n' + 'Response Message: ' + response.statusMessage + '\n' + 'Content Type: ' + response.headers['content-type']);
+        console.log('Downloading image...'); 
       })
       .on('end', function (){
-        console.log('Downloaded image!')
+        console.log('Downloaded image!');
       })
       .pipe(fs.createWriteStream('./future.jpg'));     // Note 4
+
+
